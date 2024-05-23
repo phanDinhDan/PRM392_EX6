d4ccf7f4facfdd57530e30afc0fd9417c2284cfc
phanDinhDan <phandinhdan6602@gmail.com>
2024-05-23 13:27:42 +0700

    fitst commit

---
+++ app/src/main/java/com/dinhdan/prm_ex6/MainActivity.java
@@ -0,0 +1,57 @@
+package com.dinhdan.prm_ex6;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main
+        ), (v, insets) -> {
+            Insets systemBars =
+                    insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top,
+                    systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Log.i("MainActivity", "State: Created");
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity", "State: Started");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity", "State: Resumed");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("MainActivity", "State: Paused");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity", "State: Stopped");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity", "State: Destroyed");
+    }
+
+}
